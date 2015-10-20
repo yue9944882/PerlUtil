4fe71b65411f20064a972b6b90633baa10885188
yue9944882 <291271447@qq.com>
2015-10-20 10:39:20 -0400

    Check Polynomial Format Roughly

--- Math/Polynomial.pm
+++ Math/Polynomial.pm
@@ -10,19 +10,50 @@ require Exporter;
 
 our @ISA= qw/Exporter/;
 
-our @EXPORTER=qw/hello/;
+our @EXPORT=qw/parse_poly check_poly/;
 
-our %EXPORTER_TAGS=(
+our @EXPORT_OK=qw/parse_poly check_poly/;
+
+our %EXPORT_TAGS=(
 	
 );
 
 
-sub hello{
-	print 'good';
+#Code Defination Begin
+
+sub parse_poly ($){
+	
+	my $poly=shift;
+
+	my $check=&check_poly($poly);
+	
+	#Check if the given parameter is a polynomial
+
+	unless($check){die 'Parsing Process Cannot Continue..\nExiting...\n';}
+
+	#Parsing Polynomial
+}
+
+
+sub check_poly($){
+	my $poly=shift;
+
+	if($poly=~/^(.+)=([ \(\)\*\/+-^x\d ]+)$/x){
+		print "Check Polynomial Sucess!\n";
+		return undef;
+	}else{
+		print "Check Polynomial Failed!\n";
+		return 1;
+	}
 }
 
 
 
+
+
+1;
+
+__END__
 
 
 
