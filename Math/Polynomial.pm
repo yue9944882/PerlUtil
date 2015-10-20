c955217604ecdf1637f51209114cff24d6f1ae98
Kimmin <291271447@qq.com>
2015-10-21 00:25:47 +0800

    Calc Polynomial

--- Math/Polynomial.pm
+++ Math/Polynomial.pm
@@ -4,6 +4,8 @@ package PerlUtil::Math::Polynomial;
 use strict;
 use warnings;
 
+use Regexp::Common;
+
 our $VERSION='1.00';
 
 require Exporter;
@@ -21,9 +23,13 @@ our %EXPORT_TAGS=(
 
 #Code Defination Begin
 
-sub parse_poly ($){
+my $num_reg=$RE{num}{real};
+my $op_reg=qr/[+-*/^]/;
+
+sub parse_poly ($$){
 	
 	my $poly=shift;
+	my $x_var=shift;
 
 	my $check=&check_poly($poly);
 	
@@ -35,12 +41,36 @@ sub parse_poly ($){
 
 
 	#Parsing Polynomial
+	
+	
+	
 }
 
 #Recursively Parse Polynomial with brancket
 
-sub parse_brck{
+sub parse_brck($$){
+	
+	my $sub_poly=shift;
+	my $x_var=shift;
+	
+	my $in_brck;
+	
+	while($sub_poly=~/\( (.*) \)/gx){
 		
+		$in_brck=$1;
+		&parse_brck($sub_poly,$x_var);
+		
+	}
+	
+	if($in_brck=~s/x/$x_var/){
+		print "x->$x_var: $in_brck"."\n";
+	}
+	
+	if($in_brck=~/($num_reg)\s*($op_reg)\s*($num_reg)/x){
+		my $ans=&calc_poly($2,$1,$3);
+		
+	}
+	
 }
 
 sub check_poly($){
@@ -56,7 +86,28 @@ sub check_poly($){
 }
 
 
+sub calc_poly($$$){
+	my $op=shift;
+	my $num1=shift;
+	my $num2=shift;
 
+	my $ret;
+	
+	if($op eq '+'){
+		$ret=$num1+$num2;	
+	}elsif($op eq '-'){
+		$ret=$num1-$num2;
+	}elsif($op eq '*'){
+		$ret=$num1*$num2;
+	}elsif($op eq '/'){
+		$ret=$num1/$num2;
+	}elsif($op eq '^'){
+		$ret=$num1**$num2;
+	}else{
+		die 'Operator Recgnization Error!';
+	}
+	
+}
 
 
 1;
