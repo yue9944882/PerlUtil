3501934edcc7f2346264d9dbbd2126f19e28639e
yue9944882 <291271447@qq.com>
2015-10-20 09:23:06 -0400

    Module Test

---
+++ Math/Polynomial.pm
@@ -0,0 +1,30 @@
+
+package PerlUtil::Math::Polynomial;
+
+use strict;
+use warnings;
+
+our $VERSION='1.00';
+
+require Exporter;
+
+our @ISA= qw/Exporter/;
+
+our @EXPORTER=qw/hello/;
+
+our %EXPORTER_TAGS=(
+	
+);
+
+
+sub hello{
+	print 'good';
+}
+
+
+
+
+
+
+
+
